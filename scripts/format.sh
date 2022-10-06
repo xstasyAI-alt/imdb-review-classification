0b5ccd63bc70934726611d72fd4efdf772063790
dminhvu <minhngoc02hn@gmail.com>
2022-10-06 08:56:08 +0700

    update data

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --ignore-init-module-imports --remove-all-unused-imports --recursive --remove-unused-variables --in-place app common --exclude='*pb*'
-black app common
-isort app common
+autoflake --ignore-init-module-imports --remove-all-unused-imports --recursive --remove-unused-variables --in-place src --exclude='*pb*'
+black src
+isort src
